826586de0349c47d541ef2ef5bcf914cf22d3538
Andrei Doca <docaandrei62@yahoo.com>
2026-03-27 22:35:30 +0200

    git pushfeat(sandbox): implement Piston API /execute endpoint commit

--- shared/src/types/index.ts
+++ shared/src/types/index.ts
@@ -2,3 +2,17 @@ export type ApiResponse = {
   message: string;
   success: true;
 }
+
+export type ExecuteRequest = {
+  language: string;
+  version: string;
+  code: string;
+}
+
+export type ExecuteResponse = {
+  success: boolean;
+  stdout: string;
+  stderr: string;
+  compileOutput?: string;
+  error?: string;
+}
